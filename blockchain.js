751c38c40197132344a422ce41806740315aded2
Neksyq <neksandrianov@gmail.com>
2024-09-30 22:11:45 +0200

    Added Proof Of Work method to the Crypto Block class

--- blockchain.js
+++ blockchain.js
@@ -9,4 +9,23 @@ class CryptoBlock {
     this.hash = this.computeHash();
     this.nonce = 0;
   }
+
+  computeHash() {
+    return SHA256(
+      this.index +
+        this.precedingHash +
+        this.timestamp +
+        JSON.stringify(this.data) +
+        this.nonce
+    ).toString();
+  }
+
+  proofOfWork(difficulty) {
+    while (
+      this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")
+    ) {
+      this.nonce++;
+      this.hash = this.computeHash();
+    }
+  }
 }
